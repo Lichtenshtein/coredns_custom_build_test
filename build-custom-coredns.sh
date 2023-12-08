c1ff652b0d3d337223b95694a83e58f805bb0ece
Alexei T <Lichtenshtein@users.noreply.github.com>
2023-12-08 14:30:12 +0000

    Update build-custom-coredns.sh

--- build-custom-coredns.sh
+++ build-custom-coredns.sh
@@ -13,14 +13,17 @@ git apply ../coredns_custom_build/forward-setup.go.patch
 sed -i.bak 's|forward:forward|fallback:github.com/missdeer/fallback\ndnsredir:github.com/leiless/dnsredir\nforward:forward\nproxy:github.com/missdeer/proxy\nhttps:github.com/v-byte-cpu/coredns-https|g' plugin.cfg
 sed -i.bak 's|hosts:hosts|ads:github.com/missdeer/ads\nblocklist:github.com/relekang/coredns-blocklist\nhosts:hosts|g' plugin.cfg
 sed -i.bak 's|rewrite:rewrite|rewrite:rewrite\nbogus:github.com/missdeer/bogus\nipset:github.com/missdeer/ipset|g' plugin.cfg
-sed -i.bak 's|cache:cache|cache:cache\nredisc:github.com/missdeer/redis|g' plugin.cfg
+sed -i.bak 's|cache:cache|cache:cache\non:github.com/coredns/caddy/onevent\nmeshname:github.com/zhoreeq/coredns-meshname\nmeship:github.com/zhoreeq/coredns-meship\nens:github.com/wealdtech/coredns-ens\nwgsd:github.com/jwhited/wgsd\nredisc:github.com/missdeer/redis|g' plugin.cfg
 echo "replace (" >> go.mod
 echo "    github.com/leiless/dnsredir => ../dnsredir" >> go.mod
 echo ")" >> go.mod
 sed -i.bak '/azure/d' plugin.cfg
 sed -i.bak '/route53/d' plugin.cfg
-sed -i.bak '/trace/d' plugin.cfg
 sed -i.bak '/etcd/d' plugin.cfg
 sed -i.bak '/clouddns/d' plugin.cfg
 sed -i.bak '/k8s_external/d' plugin.cfg
 sed -i.bak '/kubernetes/d' plugin.cfg
+sed -i.bak '/chaos/d' plugin.cfg
+sed -i.bak '/dnstap/d' plugin.cfg
+sed -i.bak '/prometheus/d' plugin.cfg
+sed -i.bak '/geoip/d' plugin.cfg
